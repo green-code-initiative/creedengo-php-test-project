2c38c2b855da1ca5f1870113579a6536a117512c
David DE CARVALHO <dedece35@gmail.com>
2023-06-07 10:50:48 +0200

    [ISSUE 121] adding other use cases on tests - implementation to complete

--- src/AvoidMultipleIfElseStatement.php
+++ src/AvoidMultipleIfElseStatement.php
@@ -1,6 +1,7 @@
 <?php
 class AvoidMultipleIfElseStatement
 {
+
     public function methodWithMultipleIfElseIf()
     {
         $nb1 = 0;
@@ -26,17 +27,18 @@ class AvoidMultipleIfElseStatement
         if ($nb1 == 1) {
             $nb1 = 1;
         } else {
-            //
-        } // NOK {{Use a switch statement instead of multiple if-else if possible}}
-        if ($nb1 == 1) {
-            $nb1 = 1;
-        } else {
             $nb1 = 2;
+        } // NOK {{Use a switch statement instead of multiple if-else if possible}}
+
+        if ($nb1 == 2) {
+            $nb1 = 3;
+        } else {
+            $nb1 = 4;
         } // NOK {{Use a switch statement instead of multiple if-else if possible}}
 
         return $nb1;
     }
-    
+
     public function methodWithOneIfElseIf()
     {
         $nb1 = 0;
@@ -58,9 +60,89 @@ class AvoidMultipleIfElseStatement
         $nb1 = 0;
 
         if ($nb1 == 1) {
+            $nb1 = 2;
+        } else {
+            $nb1 = 3;
+        }
+
+        return $nb1;
+    }
+
+    public function methodWithMultipleIfElseDifferentVariables() // Compliant (2 if on different variables)
+    {
+        $nb1 = 0;
+        $nb2 = 0;
+
+        if ($nb1 == 1) {
             $nb1 = 1;
         } else {
-            $nb1 = 2;
+            $nb2 = 2;
+        }
+
+        if ($nb2 == 2) {
+            $nb1 = 3;
+        } else {
+            $nb1 = 4;
+        }
+
+        return $nb1;
+    }
+
+    public function methodWithMultipleIfElseIfSeparated() // Compliant (only 2 if on the same variable)
+    {
+        $nb1 = 0;
+
+        if ($nb1 == 1) {
+            $nb1 = 1;
+        } else {
+            if ($nb1 == 2) {
+                $nb1 = 3;
+            } else {
+                $nb1 = 2;
+            }
+        }
+
+        return $nb1;
+    }
+
+    public function methodWithMultipleIfElseIfSeparatedDeep()
+    {
+        $nb1 = 0;
+
+        if ($nb1 == 1) {
+            $nb1 = 1;
+        } else {
+            if ($nb1 == 2) {
+                $nb1 = 3;
+            } else {
+                if ($nb1 == 3) {
+                    $nb1 = 4;
+                } else {
+                    $nb1 = 5;
+                }
+            }
+        } // NOK {{Use a switch statement instead of multiple if-else if possible}}
+
+        return $nb1;
+    }
+
+    public function methodWithMultipleIfElseIfSeparatedDeepButDifferentVariables() // Compliant
+    {
+        $nb1 = 0;
+        $nb2 = 0;
+
+        if ($nb1 == 1) {
+            $nb1 = 1;
+        } else {
+            if ($nb1 == 2) {
+                $nb1 = 3;
+            } else {
+                if ($nb2 == 2) {
+                    $nb1 = 3;
+                } else {
+                    $nb1 = 2;
+                }
+            }
         }
 
         return $nb1;
